a04e7650236b606358df0e5f49b0a116bfb390ce
Cooper Nichols <coopnich92@gmail.com>
2020-02-13 10:40:55 -0600

    HTML factory created and interests render to dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,13 @@
-// TODO  Set up interface in HTML with forms and labels
+import API from "./api.js"
+import renderInterests from "./render.js"
+
+
+// TODO  ! Set up interface in HTML with forms and labels
 //       Set up fetch call to retrieve data
 //       create HTML factory for data
 //       create function to render data to DOM
+
+API.getInterests()
+.then(parsedEntries => {
+    renderInterests(parsedEntries)
+ })
